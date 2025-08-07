ecf06cb24713d3208d5ca41c9e786ed517cd54d6
Milana-Rzazade <71l1c0h@code.edu.az>
2025-08-07 10:27:53 -0400

    Add fetch() function

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ const putform =document.getElementById("putform");
 const output = document.getElementById("output");
 const message = document.getElementById("message");
 
+//fetch()
+
+fetchbtn.addEventListener("click", () => {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then(response => {
+        if(!response.ok) throw new Error("The server response failed.");
+        return response.json();
+    })
+    .then(data => {
+        output.innerText = `Title: ${data.title}\nContent: ${data.body}`;
+        message.innerText = "";
+    })
+    .catch(error => {
+        message.innerText = `ERROR (fetch): ${error.message}`;
+    });
+});
