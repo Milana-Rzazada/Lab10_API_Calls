7b33fba3ebd9853373c0aa32333dfc51aa87c80e
Milana-Rzazade <71l1c0h@code.edu.az>
2025-08-07 10:16:40 -0400

    Add DOM elements.

---
+++ script.js
@@ -0,0 +1,7 @@
+const fetchbtn = document.getElementById("fetchbtn");
+const xhrbtn = document.getElementById("xhrbtn");
+const postform = document.getElementById("postform");
+const putform =document.getElementById("putform");
+const output = document.getElementById("output");
+const message = document.getElementById("message");
+
